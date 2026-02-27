e61419e45c22f7532b95536dbd3d545a19617bb4
fayudistira <fayudistiraasnan@gmail.com>
2026-02-27 14:13:18 +0700

    localize bootstrapp

--- app/Modules/Frontend/Views/layout.php
+++ app/Modules/Frontend/Views/layout.php
@@ -62,8 +62,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/bootstrap-icons.css">
     
     <style>
         :root {
@@ -504,7 +504,7 @@
         </div>
     </footer>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="/assets/js/bootstrap.bundle.min.js"></script>
     <?= $this->renderSection('extra_scripts') ?>
 
     <!-- Admission Popup Notification -->
