49582dff472d80c79c765e4e6e30a30c43e45627
fayudistira <fayudistiraasnan@gmail.com>
2026-03-12 13:56:13 +0700

    x

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -8,8 +8,7 @@ use CodeIgniter\Router\RouteCollection;
 // Default route is handled by Frontend module
 // $routes->get('/', 'Home::index');
 
-// Redirect old formulir-pendaftaran-mandarin route to /mandarin
-// $routes->get('public/formulir-pendaftaran-mandarin', redirect('/mandarin', 301));
+
 
 // Register Shield routes except login and register (we'll override those)
 // Wrap in try-catch to prevent errors if Shield service isn't ready
